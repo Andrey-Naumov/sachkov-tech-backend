4b54a5d4baf37bcc8e035e54116fdf30278063ed
kirill_sachkov <sachkovkrl@gmail.com>
2025-03-19 13:00:46 +0300

    some fixes

--- IssueService/src/Issues/SachkovTech.Issues.Application/Features/GrpcNotificationServiceTester/Commands/AddNotificationSettings/AddNotificationSettingsCommand.cs
+++
@@ -1,8 +0,0 @@
-﻿using SachkovTech.Core.Abstractions;
-
-namespace SachkovTech.Issues.Application.Features.GrpcNotificationServiceTester.Commands.AddNotificationSettings;
-
-public record AddNotificationSettingsCommand(
-    Guid UserId,
-    string Email,
-    string? WebEndpoint) : ICommand;
