c7a2bf61915c6d9742ef0cbc02dc3253ba9f632a
kirill_sachkov <sachkovkrl@gmail.com>
2025-03-26 11:50:53 +0300

    add cors

--- AccountService/src/AccountService.Api/Extensions/WebApplicationExtensions.cs
+++ AccountService/src/AccountService.Api/Extensions/WebApplicationExtensions.cs
@@ -32,7 +32,10 @@ public static class WebApplicationExtensions
     {
         app.UseCors(config =>
         {
-            config.WithOrigins("http://localhost:5173", "http://localhost:4173")
+            config.WithOrigins(
+                    "http://localhost:5173",
+                    "http://localhost:4173",
+                    "http://192.168.1.222:4173")
                 .AllowCredentials()
                 .AllowAnyHeader()
                 .AllowAnyMethod();
